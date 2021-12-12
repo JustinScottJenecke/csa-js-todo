58c6dd29ce2e1f578d35859049ef8339f8ba9e3c
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2021-12-12 09:05:22 +0200

    completes part 2 of todo tut

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,88 @@
+
+// Todo Item Class
 class TodoItem {
     constructor(name, date) {
 
         this._name = name;
         this._date = date;
-        this._completed = true;
+        this._completed = false;
     }
 
-    get getName() {
-        return this._name;
+    get getCompleted() {
+        return this._completed;
     }
 
-    static isComplete(todoItem) {
-        return todoItem._name;
+    getCompleted() {
+        this._completed = !this._completed;
     }
+}
+
+/*---------------------------
+    Setup
+---------------------------*/
+
+// Test Class Object
+let y = new TodoItem("Make Coffee", "Tonight")
+
+//DataStore
+const dataStore = [];
+
+//Entry point to tasks
+const taskList = document.getElementById("taskList");
+
+const createElement = (item) => {
+    return `<li class="activeTask" > ${item._name} ${item._date} ${item._completed} </li>`;
+}
+
+/*----------------------------
+    CRUD Functions:
+-----------------------------*/
+
+const createItem = () => {
+    console.log("s")
+
+    const submit = document.getElementById("submit");
+    submit.onclick = (event) => {
+
+        event.preventDefault();
+        //Form
+        /*const inputForm = document.getElementById("inputForm");
+
+        inputForm.preventDefault();*/
+
+        //taskName taskDate
+        const inputName = document.getElementById("taskName");
+        const inputDate = document.getElementById("taskDate");
+        
+        let newTodoItem = new TodoItem(inputName, inputDate);
+
+        console.log(newTodoItem);
+
+
+    }
+
+}
+
+const deleteItem = () => {
+    console.log("s")
+}
+
+const readItems = () => {
+    console.log("s")
 }
 
 
 
-let yes = "true";
-
-let y = new TodoItem("Make Coffee", "Tonight")
-
-const taskList = document.getElementById("taskList");
-
-let x = document.getElementsByClassName("control__button");
+/*
+let cBtn = document.getElementsByClassName("control__button");
 
 for (let index = 0; index < 3; index++) {
 
-    const eachBtn = x[index];
+    const eachBtn = cBtn[index];
 
     eachBtn.addEventListener("click", (event) => {
 
-        taskList.innerHTML += "<li> " + y._name + " | " +  y._date + " | " + y._completed + " </li>";
-
+        taskList.innerHTML += `<li class="todoItem"> ${y._name} | ${y._date} | ${y._completed} </li>`;
+        console.log(event);
     })
-}
-/*
-
-    new Vue {(
-        data : {
-        newItem : "";
-        }
-    )}
-
-*/
+}*/
